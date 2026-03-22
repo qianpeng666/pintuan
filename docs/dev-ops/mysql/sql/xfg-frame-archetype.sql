7dd1e1c985ae059cce973b121028f24f79f5d274
cyy <qianp0424@gmail.com>
2026-03-22 18:12:37 +0800

    feat：第一节测试

--- docs/dev-ops/mysql/sql/xfg-frame-archetype.sql
+++ docs/dev-ops/mysql/sql/xfg-frame-archetype.sql
@@ -1,108 +1,70 @@
-/*
- Navicat Premium Data Transfer
+CREATE database if NOT EXISTS `group_buy_market` default character set utf8mb4 collate utf8mb4_0900_ai_ci;
+use `group_buy_market`;
 
- Source Server         : 127.0.0.1
- Source Server Type    : MySQL
- Source Server Version : 50639
- Source Host           : localhost:3306
- Source Schema         : road-map
+# 转储表 group_buy_activity
+# ------------------------------------------------------------
 
- Target Server Type    : MySQL
- Target Server Version : 50639
- File Encoding         : 65001
+DROP TABLE IF EXISTS `group_buy_activity`;
 
- Date: 15/07/2023 09:26:39
-*/
+CREATE TABLE `group_buy_activity` (
+                                      `id` bigint(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增',
+                                      `activity_id` bigint(8) NOT NULL COMMENT '活动ID',
+                                      `source` varchar(8) NOT NULL COMMENT '来源',
+                                      `channel` varchar(8) NOT NULL COMMENT '渠道',
+                                      `goods_id` varchar(12) NOT NULL COMMENT '商品ID',
+                                      `discount_id` varchar(8) NOT NULL COMMENT '折扣ID',
+                                      `group_type` tinyint(1) NOT NULL DEFAULT '0' COMMENT '拼团方式（0自动成团、1达成目标拼团）',
+                                      `take_limit_count` int(4) NOT NULL DEFAULT '1' COMMENT '拼团次数限制',
+                                      `target` int(5) NOT NULL DEFAULT '1' COMMENT '拼团目标',
+                                      `valid_time` int(4) NOT NULL DEFAULT '15' COMMENT '拼团时长（分钟）',
+                                      `status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '活动状态（0创建、1生效、2过期、3废弃）',
+                                      `start_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '活动开始时间',
+                                      `end_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '活动结束时间',
+                                      `tag_id` varchar(8) DEFAULT NULL COMMENT '人群标签规则标识',
+                                      `tag_scope` varchar(4) DEFAULT NULL COMMENT '人群标签规则范围（多选；1可见限制、2参与限制）',
+                                      `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                                      `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+                                      PRIMARY KEY (`id`),
+                                      UNIQUE KEY `uq_activity_id` (`activity_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='拼团活动';
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+LOCK TABLES `group_buy_activity` WRITE;
+/*!40000 ALTER TABLE `group_buy_activity` DISABLE KEYS */;
 
-CREATE database if NOT EXISTS `xfg_frame_archetype` default character set utf8mb4 collate utf8mb4_0900_ai_ci;
-use `xfg_frame_archetype`;
+INSERT INTO `group_buy_activity` (`id`, `activity_id`, `source`, `channel`, `goods_id`, `discount_id`, `group_type`, `take_limit_count`, `target`, `valid_time`, `status`, `start_time`, `end_time`, `tag_id`, `tag_scope`, `create_time`, `update_time`)
+VALUES
+    (1,100123,'s01','c01','9890001','25120207',0,1,1,15,0,'2024-12-07 10:19:40','2024-12-07 10:19:40','1','1','2024-12-07 10:19:40','2024-12-07 10:19:40');
 
--- ----------------------------
--- Table structure for employee
--- ----------------------------
-DROP TABLE IF EXISTS `employee`;
-CREATE TABLE `employee` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `employee_number` varchar(16) NOT NULL DEFAULT '' COMMENT '雇员ID',
-  `employee_name` varchar(32) NOT NULL DEFAULT '' COMMENT '雇员姓名',
-  `employee_level` varchar(8) NOT NULL DEFAULT '' COMMENT '雇员级别',
-  `employee_title` varchar(16) NOT NULL DEFAULT '' COMMENT '雇员岗位Title',
-  `create_time` datetime NOT NULL COMMENT '创建时间',
-  `update_time` datetime NOT NULL COMMENT '更新时间',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `idx_employee_number` (`employee_number`)
-) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8;
+/*!40000 ALTER TABLE `group_buy_activity` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- ----------------------------
--- Records of employee
--- ----------------------------
-BEGIN;
-INSERT INTO `employee` VALUES (1, '10000001', 'sXvfDpsWnJdLsCVk64tJgw==', 'T-3', '中级工程师', '2023-07-14 15:26:26', '2023-07-14 15:26:26');
-INSERT INTO `employee` VALUES (2, '10000010', 'sXvfDpsWnJdLsCVk64tJgw==', 'T2', '见习工程师', '2023-07-14 15:34:40', '2023-07-14 15:34:40');
-INSERT INTO `employee` VALUES (3, '10000011', 'sXvfDpsWnJdLsCVk64tJgw==', 'T2', '见习工程师', '2023-07-14 15:34:40', '2023-07-14 15:34:40');
-INSERT INTO `employee` VALUES (4, '10000012', 'sXvfDpsWnJdLsCVk64tJgw==', 'T2', '见习工程师', '2023-07-14 15:34:40', '2023-07-14 15:34:40');
-INSERT INTO `employee` VALUES (5, '10000013', 'sXvfDpsWnJdLsCVk64tJgw==', 'T2', '见习工程师', '2023-07-14 15:34:40', '2023-07-14 15:34:40');
-INSERT INTO `employee` VALUES (6, '10000014', 'sXvfDpsWnJdLsCVk64tJgw==', 'T2', '见习工程师', '2023-07-14 15:34:40', '2023-07-14 15:34:40');
-INSERT INTO `employee` VALUES (9, '10000002', 'sXvfDpsWnJdLsCVk64tJgw==', 'T2', '见习工程师', '2023-07-15 07:42:52', '2023-07-15 07:42:52');
-INSERT INTO `employee` VALUES (22, '10000015', 'hMCgLG6WV3CsNBQ1UD6PEQ==', 'T2', '见习工程师', '2023-07-15 08:02:31', '2023-07-15 08:02:31');
-INSERT INTO `employee` VALUES (23, '10000016', 'hMCgLG6WV3CsNBQ1UD6PEQ==', 'T2', '见习工程师', '2023-07-15 08:02:31', '2023-07-15 08:02:31');
-INSERT INTO `employee` VALUES (24, '10000017', 'hMCgLG6WV3CsNBQ1UD6PEQ==', 'T2', '见习工程师', '2023-07-15 08:02:31', '2023-07-15 08:02:31');
-INSERT INTO `employee` VALUES (39, '10000022', 'GyG+V0r6mBCNsdusuKl03g==', 'T1', '实习工程师', '2023-07-15 09:17:49', '2023-07-15 09:17:49');
-COMMIT;
 
--- ----------------------------
--- Table structure for employee_salary
--- ----------------------------
-DROP TABLE IF EXISTS `employee_salary`;
-CREATE TABLE `employee_salary` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `employee_number` varchar(16) NOT NULL DEFAULT '' COMMENT '雇员编号',
-  `salary_total_amount` decimal(8,2) NOT NULL COMMENT '薪资总额',
-  `salary_merit_amount` decimal(8,2) NOT NULL COMMENT '绩效工资',
-  `salary_base_amount` decimal(8,2) NOT NULL COMMENT '基础工资',
-  `create_time` datetime NOT NULL COMMENT '创建时间',
-  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
-  PRIMARY KEY (`id`),
-  KEY `idx_employee_number` (`employee_number`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+# 转储表 group_buy_discount
+# ------------------------------------------------------------
 
--- ----------------------------
--- Records of employee_salary
--- ----------------------------
-BEGIN;
-INSERT INTO `employee_salary` VALUES (1, '10000001', 5100.00, 1020.00, 4080.00, '2023-07-14 16:09:06', '2023-07-14 16:09:06');
-INSERT INTO `employee_salary` VALUES (2, '10000010', 5000.00, 1000.00, 4000.00, '2023-07-14 16:17:10', '2023-07-14 16:17:10');
-INSERT INTO `employee_salary` VALUES (3, '10000011', 5000.00, 1000.00, 4000.00, '2023-07-14 16:17:10', '2023-07-14 16:17:10');
-INSERT INTO `employee_salary` VALUES (4, '10000012', 5000.00, 1000.00, 4000.00, '2023-07-14 16:17:10', '2023-07-14 16:17:10');
-INSERT INTO `employee_salary` VALUES (5, '10000013', 5000.00, 1000.00, 4000.00, '2023-07-14 16:17:10', '2023-07-14 16:17:10');
-INSERT INTO `employee_salary` VALUES (6, '10000014', 5000.00, 1000.00, 4000.00, '2023-07-14 16:17:10', '2023-07-14 16:17:10');
-INSERT INTO `employee_salary` VALUES (8, '10000022', 100.00, 10.00, 90.00, '2023-07-15 09:17:49', '2023-07-15 09:17:49');
-COMMIT;
+DROP TABLE IF EXISTS `group_buy_discount`;
 
--- ----------------------------
--- Table structure for employee_salary_adjust
--- ----------------------------
-DROP TABLE IF EXISTS `employee_salary_adjust`;
-CREATE TABLE `employee_salary_adjust` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `employee_number` varchar(16) NOT NULL DEFAULT '' COMMENT '雇员编号',
-  `adjust_order_id` varchar(32) NOT NULL DEFAULT '' COMMENT '调薪单号',
-  `adjust_total_amount` decimal(8,2) NOT NULL COMMENT '总额调薪',
-  `adjust_base_amount` decimal(8,2) NOT NULL COMMENT '基础调薪',
-  `adjust_merit_amount` decimal(8,2) NOT NULL COMMENT '绩效调薪',
-  `create_time` datetime NOT NULL COMMENT '创建时间',
-  `update_time` datetime NOT NULL COMMENT '更新时间',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `idx_order_id` (`adjust_order_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+CREATE TABLE `group_buy_discount` (
+                                      `id` bigint(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+                                      `discount_id` int(8) NOT NULL COMMENT '折扣ID',
+                                      `discount_name` varchar(64) NOT NULL COMMENT '折扣标题',
+                                      `discount_desc` varchar(256) NOT NULL COMMENT '折扣描述',
+                                      `discount_type` tinyint(1) NOT NULL DEFAULT '0' COMMENT '折扣类型（0:base、1:tag）',
+                                      `market_plan` varchar(4) NOT NULL DEFAULT 'ZJ' COMMENT '营销优惠计划（ZJ:直减、MJ:满减、N元购）',
+                                      `market_expr` varchar(32) NOT NULL COMMENT '营销优惠表达式',
+                                      `tag_id` varchar(8) DEFAULT NULL COMMENT '人群标签，特定优惠限定',
+                                      `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                                      `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+                                      PRIMARY KEY (`id`),
+                                      UNIQUE KEY `uq_discount_id` (`discount_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- ----------------------------
--- Records of employee_salary_adjust
--- ----------------------------
-BEGIN;
-INSERT INTO `employee_salary_adjust` VALUES (1, '10000001', '109089990198888811', 1000.00, 800.00, 200.00, '2023-07-14 16:55:53', '2023-07-14 16:55:53');
-INSERT INTO `employee_salary_adjust` VALUES (2, '10000001', '100908977676001', 100.00, 20.00, 80.00, '2023-07-14 21:57:39', '2023-07-14 21:57:39');
-COMMIT;
+LOCK TABLES `group_buy_discount` WRITE;
+/*!40000 ALTER TABLE `group_buy_discount` DISABLE KEYS */;
+
+INSERT INTO `group_buy_discount` (`id`, `discount_id`, `discount_name`, `discount_desc`, `discount_type`, `market_plan`, `market_expr`, `tag_id`, `create_time`, `update_time`)
+VALUES
+    (1,9890001,'测试优惠','测试优惠',0,'ZJ','20',NULL,'2024-12-07 10:20:15','2024-12-07 10:20:15');
+
+/*!40000 ALTER TABLE `group_buy_discount` ENABLE KEYS */;
+UNLOCK TABLES;
---
+++ pintuan-infrastructure/src/main/java/com/example/myapp/infrastructure/dao/IGroupBuyDiscountDao.java
@@ -0,0 +1,12 @@
+package com.example.myapp.infrastructure.dao;
+
+
+import com.example.myapp.infrastructure.dao.po.GroupBuyDiscount;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface IGroupBuyDiscountDao {
+    List<GroupBuyDiscount> queryGroupBuyDiscountList();
+}
